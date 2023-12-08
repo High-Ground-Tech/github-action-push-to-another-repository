8761b1a2eda84189fbb2209cd91554bd29ed7cbb
ValeriyBogorodskiy <84142138+ValeriyBogorodskiy@users.noreply.github.com>
2023-12-08 23:22:57 +0400

    Fix cp command option
    Error: the -R and -r options may not be specified together

--- entrypoint.sh
+++ entrypoint.sh
@@ -120,7 +120,7 @@ then
 fi
 
 echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $TARGET_DIRECTORY in git repo $DESTINATION_REPOSITORY_NAME"
-cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
+cp -r "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
 cd "$CLONE_DIR"
 
 echo "[+] Files that will be pushed"
